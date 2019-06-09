df36a90a539268be9530ac6cd12199c12b3886e9
tipmvp <50410377+tipmvp@users.noreply.github.com>
2019-06-09 17:21:41 +0800

    Update User.java

--- src/main/java/boot/pojo/User.java
+++ src/main/java/boot/pojo/User.java
@@ -23,6 +23,5 @@ public class User implements Serializable {
     private String userRoleName;    //用户角色名称
     private String idPicPath; //证件照路径
     private String workPicPath; //工作照路径
-    private String wee;
 
 }
